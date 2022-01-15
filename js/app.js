951163e37efa0abc52e42a5f9d4475008393a9b5
Rochelle Hickey <rochellehickey@gmail.com>
2022-01-15 14:34:19 -0500

    Initial cleanup of files from WP

--- js/app.js
+++ js/app.js
@@ -1,237 +1,106 @@
-/*jshint esversion: 6 */
+/**
+ * File navigation.js.
+ *
+ * Handles toggling the navigation menu for small screens and enables TAB key
+ * navigation support for dropdown menus.
+ */
+( function() {
+  var container, button, menu, links, i, len;
 
-// sticky header
-const body = document.body;
-const scrollUp = "scroll-up";
-const scrollDown = "scroll-down";
-let lastScroll = 0;
-// filter button
-const button = document.getElementsByTagName('BUTTON');
-const resetFilter = document.getElementById('reset');
-const cssFilter = document.getElementById('cssFilter');
-const jsFilter = document.getElementById('jsFilter');
-const reactFilter = document.getElementById('reactFilter');
-const reportsFilter = document.getElementById('reportsFilter');
-const multipleComponentsFilter = document.getElementById('multipleComponentsFilter');
-const infographicsFilter = document.getElementById('infographicsFilter');
-
-
-// STICKY HEADER
-
-// When the page scrolls
-window.addEventListener("scroll", () => {
-  // Save the current scroll position
-  const currentScroll = window.pageYOffset;
-  // If the current scroll is at the top, remove the scrollUp class
-  if (currentScroll == 0) {
-    body.classList.remove(scrollUp);
+  container = document.getElementById( 'site-navigation' );
+  if ( ! container ) {
     return;
   }
 
-  if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
-    // Check if the current scroll is higher than the lastScroll and check if the body does not have the class scrollDown
-    // remove class scrollUp
-    // add class scrollDown
-    body.classList.remove(scrollUp);
-    body.classList.add(scrollDown);
-  } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
-    // Check if the current scroll is less than the lastScroll and check if the body does have the class scrollDown
-    // remove class scrollDown
-    // add class scrollUp
-    body.classList.remove(scrollDown);
-    body.classList.add(scrollUp);
+  button = container.getElementsByTagName( 'button' )[0];
+  if ( 'undefined' === typeof button ) {
+    return;
   }
-  // update the lastScroll to equal the currentScroll
-  lastScroll = currentScroll;
-});
 
+  menu = container.getElementsByTagName( 'ul' )[0];
 
-// FILTER BUTTONS
+  // Hide menu toggle button if menu is empty and return early.
+  if ( 'undefined' === typeof menu ) {
+    button.style.display = 'none';
+    return;
+  }
 
-for (let i=0; i < button.length; i++) {
-  button[i].addEventListener('click', function() {
-    if (button[i] === resetFilter) {
-      // find the all the projects with the class project
-      let projects = document.querySelectorAll('.project');
-      // loop through each project found
-      for (let i = 0; i < projects.length; i++) {
-        // remove hidden class to all projects
-        // add show class to all projects
-        projects[i].classList.remove('hidden');
-        projects[i].classList.add('show');
-        // go through each button and remove the button Selected class
-        for (let i=0; i < button.length; i++) {
-          button[i].classList.remove('buttonSelected');
+  menu.setAttribute( 'aria-expanded', 'false' );
+  if ( -1 === menu.className.indexOf( 'nav-menu' ) ) {
+    menu.className += ' nav-menu';
+  }
+
+  button.onclick = function() {
+    if ( -1 !== container.className.indexOf( 'toggled' ) ) {
+      container.className = container.className.replace( ' toggled', '' );
+      button.setAttribute( 'aria-expanded', 'false' );
+      menu.setAttribute( 'aria-expanded', 'false' );
+    } else {
+      container.className += ' toggled';
+      button.setAttribute( 'aria-expanded', 'true' );
+      menu.setAttribute( 'aria-expanded', 'true' );
+    }
+  };
+
+  // Get all the link elements within the menu.
+  links    = menu.getElementsByTagName( 'a' );
+
+  // Each time a menu link is focused or blurred, toggle focus.
+  for ( i = 0, len = links.length; i < len; i++ ) {
+    links[i].addEventListener( 'focus', toggleFocus, true );
+    links[i].addEventListener( 'blur', toggleFocus, true );
+  }
+
+  /**
+   * Sets or removes .focus class on an element.
+   */
+  function toggleFocus() {
+    var self = this;
+
+    // Move up through the ancestors of the current link until we hit .nav-menu.
+    while ( -1 === self.className.indexOf( 'nav-menu' ) ) {
+
+      // On li elements toggle the class .focus.
+      if ( 'li' === self.tagName.toLowerCase() ) {
+        if ( -1 !== self.className.indexOf( 'focus' ) ) {
+          self.className = self.className.replace( ' focus', '' );
+        } else {
+          self.className += ' focus';
         }
       }
-    } else if (button[i] === cssFilter) {
-      // find the all the projects with the class project
-      let projects = document.querySelectorAll('.project');
-      // loop through each project found
-      for (let i = 0; i < projects.length; i++) {
-        // if the project does not have the data-filter attribute of css
-        if (projects[i].getAttribute('data-filter') !== "css") {
-          // remove show class
-          // add hidden class
-          projects[i].classList.remove('show');
-          projects[i].classList.add('hidden');
-        } else {
-          // if the project does have the data-filter attribute of css
-          // remove hidden class
-          // add show class
-          projects[i].classList.remove('hidden');
-          projects[i].classList.add('show');
-          // go through each button and remove the buttonSelected class
-          // add buttonSelected class to cssFilter
-          for (let i=0; i < button.length; i++) {
-            button[i].classList.remove('buttonSelected');
+
+      self = self.parentElement;
+    }
+  }
+
+  /**
+   * Toggles `focus` class to allow submenu access on tablets.
+   */
+  ( function( container ) {
+    var touchStartFn, i,
+      parentLink = container.querySelectorAll( '.menu-item-has-children > a, .page_item_has_children > a' );
+
+    if ( 'ontouchstart' in window ) {
+      touchStartFn = function( e ) {
+        var menuItem = this.parentNode, i;
+
+        if ( ! menuItem.classList.contains( 'focus' ) ) {
+          e.preventDefault();
+          for ( i = 0; i < menuItem.parentNode.children.length; ++i ) {
+            if ( menuItem === menuItem.parentNode.children[i] ) {
+              continue;
+            }
+            menuItem.parentNode.children[i].classList.remove( 'focus' );
           }
-          cssFilter.classList.add('buttonSelected');
-        }
-      }
-    } else if (button[i] === jsFilter) {
-      // find the all the projects with the class project
-      let projects = document.querySelectorAll('.project');
-      // loop through each project found
-      for (let i = 0; i < projects.length; i++) {
-        // if the project does not have the data-filter attribute of javascript
-        if (projects[i].getAttribute('data-filter') !== "javascript") {
-          // remove show class
-          // add hidden class
-          projects[i].classList.remove('show');
-          projects[i].classList.add('hidden');
+          menuItem.classList.add( 'focus' );
         } else {
-          // if the project does have the data-filter attribute of javascript
-          // remove hidden class
-          // add show class
-          projects[i].classList.remove('hidden');
-          projects[i].classList.add('show');
-          // go through each button and remove the buttonSelected class
-          // add buttonSelected class to jsFilter
-          for (let i=0; i < button.length; i++) {
-            button[i].classList.remove('buttonSelected');
-          }
-          jsFilter.classList.add('buttonSelected');
+          menuItem.classList.remove( 'focus' );
         }
-      }
-    } else if (button[i] === reactFilter) {
-      // find the all the projects with the class project
-      let projects = document.querySelectorAll('.project');
-      // loop through each project found
-      for (let i = 0; i < projects.length; i++) {
-        // if the project does not have the data-filter attribute of react
-        if (projects[i].getAttribute('data-filter') !== "react") {
-          // remove show class
-          // add hidden class
-          projects[i].classList.remove('show');
-          projects[i].classList.add('hidden');
-        } else {
-          // if the project does have the data-filter attribute of react
-          // remove hidden class
-          // add show class
-          projects[i].classList.remove('hidden');
-          projects[i].classList.add('show');
-          // go through each button and remove the buttonSelected class
-          // add buttonSelected class to reactFilter
-          for (let i=0; i < button.length; i++) {
-            button[i].classList.remove('buttonSelected');
-          }
-          reactFilter.classList.add('buttonSelected');
-        }
-      }
-    } else if (button[i] === reportsFilter) {
-      // find the all the projects with the class project
-      let projects = document.querySelectorAll('.project');
-      // loop through each project found
-      for (let i = 0; i < projects.length; i++) {
-        // if the project does not have the data-filter attribute of reports
-        if (projects[i].getAttribute('data-filter') !== "reports") {
-          // remove show class
-          // add hidden class
-          projects[i].classList.remove('show');
-          projects[i].classList.add('hidden');
-        } else {
-          // if the project does have the data-filter attribute of reports
-          // remove hidden class
-          // add show class
-          projects[i].classList.remove('hidden');
-          projects[i].classList.add('show');
-          // go through each button and remove the buttonSelected class
-          // add buttonSelected class to reportsFilter
-          for (let i=0; i < button.length; i++) {
-            button[i].classList.remove('buttonSelected');
-          }
-          reportsFilter.classList.add('buttonSelected');
-        }
-      }
-    } else if (button[i] === multipleComponentsFilter) {
-      // find the all the projects with the class project
-      let projects = document.querySelectorAll('.project');
-      // loop through each project found
-      for (let i = 0; i < projects.length; i++) {
-        // if the project does not have the data-filter attribute of multiple
-        if (projects[i].getAttribute('data-filter') !== "multiple") {
-          // remove show class
-          // add hidden class
-          projects[i].classList.remove('show');
-          projects[i].classList.add('hidden');
-        } else {
-          // if the project does have the data-filter attribute of multiple
-          // remove hidden class
-          // add show class
-          projects[i].classList.remove('hidden');
-          projects[i].classList.add('show');
-          // go through each button and remove the buttonSelected class
-          // add buttonSelected class to multipleComponentsFilter
-          for (let i=0; i < button.length; i++) {
-            button[i].classList.remove('buttonSelected');
-          }
-          multipleComponentsFilter.classList.add('buttonSelected');
-        }
-      }
-    } else if (button[i] === infographicsFilter) {
-      // find the all the projects with the class project
-      let projects = document.querySelectorAll('.project');
-      // loop through each project found
-      for (let i = 0; i < projects.length; i++) {
-        // if the project does not have the data-filter attribute of infographics
-        if (projects[i].getAttribute('data-filter') !== "infographics") {
-          // remove show class
-          // add hidden class
-          projects[i].classList.remove('show');
-          projects[i].classList.add('hidden');
-        } else {
-          // if the project does have the data-filter attribute of infographics
-          // remove hidden class
-          // add show class
-          projects[i].classList.remove('hidden');
-          projects[i].classList.add('show');
-          // go through each button and remove the buttonSelected class
-          // add buttonSelected class to infographicsFilter
-          for (let i=0; i < button.length; i++) {
-            button[i].classList.remove('buttonSelected');
-          }
-          infographicsFilter.classList.add('buttonSelected');
-        }
+      };
+
+      for ( i = 0; i < parentLink.length; ++i ) {
+        parentLink[i].addEventListener( 'touchstart', touchStartFn, false );
       }
     }
-  });
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  }( container ) );
+} )();
